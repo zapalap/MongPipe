41b4efbabf31f53b7920af97ea15499236886d42
zapalap <przemyslaw.zapala@gmail.com>
2017-10-22 19:22:38 +0200

    Add basic GROK like regex pattern resolver

---
+++ MongPipe.Core/Helpers/Regex/RegexAlias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongPipe.Core.Helpers.Regex
+{
+    public class RegexAlias
+    {
+        public string Alias { get; }
+        public string Pattern { get; }
+
+        public RegexAlias(string alias, string pattern)
+        {
+            Alias = alias;
+            Pattern = pattern;
+        }
+    }
+}
---
+++ MongPipe.Tests/RegexTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MongPipe.Core.Helpers.Regex;
+
+namespace MongPipe.Tests
+{
+    [TestClass]
+    public class RegexTests
+    {
+        [TestMethod]
+        public void RegexResolverProperlyResolvesOnePatternAtTheEndOfTheInput()
+        {
+            // Arrange
+            var patterns = new List<RegexAlias>()
+            {
+                new RegexAlias("%{WORD}", "(WORD)")
+            };
+
+            var resolver = new RegexAliasResolver(patterns);
+            var input = "This is a word pattern: %{WORD}";
+
+            // Act
+            var output = resolver.Reslove(input);
+
+            // Assert 
+            Assert.AreEqual("This is a word pattern: (WORD)", output);
+        }
+
+        [TestMethod]
+        public void RegexResolverProperlyResolvesOnePatternAtTheStartOfTheInput()
+        {
+            // Arrange
+            var patterns = new List<RegexAlias>()
+            {
+                new RegexAlias("%{WORD}", "(WORD)")
+            };
+
+            var resolver = new RegexAliasResolver(patterns);
+            var input = "%{WORD} is a pattern";
+
+            // Act
+            var output = resolver.Reslove(input);
+
+            // Assert 
+            Assert.AreEqual("(WORD) is a pattern", output);
+        }
+
+        [TestMethod]
+        public void RegexResolverProperlyResolvesMultiplePatterns()
+        {
+            // Arrange
+            var patterns = new List<RegexAlias>()
+            {
+                new RegexAlias("%{WORD}", "(WORD)"),
+                new RegexAlias("%{LETTER}", "(LETTER)"),
+                new RegexAlias("%{IP}", "(IP)"),
+            };
+
+            var resolver = new RegexAliasResolver(patterns);
+            var input = "%{WORD} is a pattern, as is %{IP} and of course a %{LETTER}";
+
+            // Act
+            var output = resolver.Reslove(input);
+
+            // Assert 
+            Assert.AreEqual("(WORD) is a pattern, as is (IP) and of course a (LETTER)", output);
+        }
+    }
+}
